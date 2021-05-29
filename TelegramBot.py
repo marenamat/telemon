e2f36d0a7390000aa56cc17b919037e3d7539fb9
Maria Matejka <mq@ucw.cz>
2021-05-29 17:52:35 +0200

    The whole Telegram bot is a class

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,17 +1,24 @@
 import requests
 import sys
 
-# Load token from a private file. Only first line is used, anything else is ignored
-try:
-    tokenfile = open("telegram.token", "r")
-    token = tokenfile.read().rstrip()
-    tokenfile.close()
-except Exception as e:
-    print("Couldn't load Telegram token:", e)
-    sys.exit(2)
+class TelegramBot:
+    def __init__(self):
+        # Load token from a private file. Only first line is used, anything else is ignored
+        try:
+            tokenfile = open("telegram.token", "r")
+            self.token = tokenfile.read().rstrip()
+            tokenfile.close()
+        except Exception as e:
+            print("Couldn't load Telegram token:", e)
+            sys.exit(2)
 
-# Just a testing API call
-print("Token:", token)
-response = requests.get('https://api.telegram.org/bot' + token + '/getMe')
-print("Status:", response.status_code)
-print("Response:", response.content)
+        print("Loaded a Telegram Bot with token", self.token)
+
+    def api(self, req):
+        # Just a testing API call
+        response = requests.get('https://api.telegram.org/bot' + self.token + '/' + req)
+        print("Status:", response.status_code)
+        print("Response:", response.content)
+
+bot = TelegramBot()
+bot.api("getMe")
