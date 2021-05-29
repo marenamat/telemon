b82834ac511215277682bf5c46d29abc1da266f0
Maria Matejka <mq@ucw.cz>
2021-05-29 19:06:28 +0200

    Using the official Telegram API for echo-like test

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,24 +1,33 @@
-import requests
 import sys
+import socket
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 class TelegramBot:
-    def __init__(self):
+    def __init__(self, name=socket.gethostname()):
+        self.name = name
+
         # Load token from a private file. Only first line is used, anything else is ignored
         try:
             tokenfile = open("telegram.token", "r")
             self.token = tokenfile.read().rstrip()
             tokenfile.close()
         except Exception as e:
-            print("Couldn't load Telegram token:", e)
+            print(f"Couldn't load Telegram token: {e}")
             sys.exit(2)
 
-        print("Loaded a Telegram Bot with token", self.token)
+        self.updater = Updater(token=self.token, use_context=True)
 
-    def api(self, req):
-        # Just a testing API call
-        response = requests.get('https://api.telegram.org/bot' + self.token + '/' + req)
-        print("Status:", response.status_code)
-        print("Response:", response.content)
+        self.updater.dispatcher.add_handler(CommandHandler('start', lambda u, c: c.bot.send_message(chat_id=u.effective_chat.id, text=f"This is TeleMon instance at {self.name}.")))
+        self.updater.dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), lambda u, c: c.bot.send_message(chat_id=u.effective_chat.id, text=f"You wrote: {u.message.text}")))
 
-bot = TelegramBot()
-bot.api("getMe")
+        print(f'Loaded a Telegram Bot with token {self.token}')
+
+    def run(self):
+        self.updater.start_polling()
+
+TelegramBot().run()
